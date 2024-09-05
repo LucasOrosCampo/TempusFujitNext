7a123751421d65560a834b2f29425d3ab8987395
LUCAS <ioroscampo@gmail.com>
2024-09-05 23:35:48 +0200

    login page created and redirecting home to login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { ChangeEvent, useState } from "react";
+
+export default function Page() {
+  let [login, setLogin] = useState<string>("");
+  let [password, setPwd] = useState<string>("");
+
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <div className="space-y-6">
+          <h1>Login</h1>
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              username
+            </label>
+            <input
+              id="username"
+              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              value={login}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setLogin(e.target.value)
+              }
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              password
+            </label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPwd(e.target.value)}
+              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
