011cdd26c509fc2ca008d277c58db80a5734e9d5
lucas <ioroscampo@gmail.com>
2025-01-23 22:53:55 +0100

    added migration and changed install command

---
+++ cs_backend/Infrastructure/Migrations/20250123202714_add_note_to_session.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cs_backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_note_to_session : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "Sessions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "Sessions");
+        }
+    }
+}
